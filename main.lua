11a0a884c7c3e0f729f9c669978c2743c22ff5fc
lynutodev <124411321+lynutodev@users.noreply.github.com>
2023-02-04 01:58:40 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+local Credits = Window:NewTab("Credits")
+local Credits = Credits:NewSection("Credits")
+
+Credits:NewLabel("Made by lynutodev#1730")
+Credits:NewLabel("!!! THIS WILL NOT BE UPDATED !!!")
